d8853a7c092e92617355be29351ba8ebc3d4d58c
haider <haider.shah@hotmail.com>
2019-09-01 23:24:18 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,24 @@
-# Your Code Here
+def map(array)
+  i=0
+  out=[]
+  while i< array.size
+    out[i]=yield(array[i])
+    i+=1
+  end
+  return out
+end
+
+def reduce(array,f=0)
+  i=0
+  total=0
+  while i< array.size
+    total=yield(array[i],total)
+    i+=1
+  end
+  if total==false 
+    return false
+  end
+  total+f
+end
+
+
